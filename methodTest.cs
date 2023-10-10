9f056efb7989f41bc8332da0420919792ed317d5
Hemant Sherawat <43252065+hzs0084@users.noreply.github.com>
2023-10-10 09:30:18 -0500

    Add files via upload

---
+++ methodTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace base_functions
+{
+    class Program
+    {
+         static void method()
+    {
+        Console.WriteLine("The Method is now running");
+    }
+        static void Main(string[] args)
+        {
+            method();
+            method();
+            method();
+            method();
+            method();
+        }
+    }
+
+   
+}
