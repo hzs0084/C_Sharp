aa1b81cfdeabbd0d811e62c5d334d2edec24b883
Hemant Sherawat <43252065+hzs0084@users.noreply.github.com>
2023-10-11 11:17:31 -0500

    Add files via upload

---
+++ moduleTestTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ezFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input for the integers
+            
+            Console.WriteLine("Enter the first number.");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter the second number.");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Call the method for output
+            double answer = Divide(num1, num2);
+            Console.WriteLine(answer);
+
+        }
+
+        //This method multiplies the integers
+        static double Divide(double a, double b)
+        {
+            double result = a / b;
+            return result;
+        }
+    }
+}
