16dd3e9f224191a2a42e114d382840577988994f
Hemant Sherawat <43252065+hzs0084@users.noreply.github.com>
2023-10-11 11:22:10 -0500

    Add files via upload

---
+++ exampleOne.cs
@@ -0,0 +1,19 @@
+﻿//Create Variables 
+
+string degree = "I am currently studying";
+string name = "My name is";
+
+
+//Prompt the user for input as Degree and name
+
+Console.WriteLine("Please enter your name: ");
+string nameInput = Console.ReadLine();
+Console.WriteLine("Please enter your degree: ");
+string degreeInput = Console.ReadLine();
+
+//Output captured data
+
+Console.Write(name);
+Console.WriteLine(" " + nameInput);
+Console.Write(degree);
+Console.WriteLine(" " + degreeInput);
---
+++ exampleTwo.cs
@@ -0,0 +1,19 @@
+﻿//Varibles for the Circumference formula
+
+const double pi = 3.14;
+//double diameter;
+string diameterPrompt = "Enter the diameter for the circle: ";
+//double circumference;
+
+//Capture Diameter inputed
+
+Console.WriteLine(diameterPrompt);
+double diameter = Convert.ToDouble(Console.ReadLine());
+
+//Perform Calculations
+
+double circumference = pi * diameter;
+
+//Output Caculations
+Console.WriteLine("Here is the circumference of your circle: " + circumference);
+// Console.WriteLine(circumference);
